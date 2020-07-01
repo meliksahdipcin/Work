42cdc74f04489f7e31f1c2245aa2263d0eb0d8a5
Melikşah Dipçin <meliksahdipcin@gmail.com>
2020-07-01 22:45:42 +0300

    Initial Commit

---
+++ Calis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calis
+//
+//  Created by Melikşah on 1.07.2020.
+//  Copyright © 2020 Melikşah Dipçin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
