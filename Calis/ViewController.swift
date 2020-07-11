a3e95455c193201477f1b2d2a77f1018c260056b
Melikşah Dipçin <meliksahdipcin@gmail.com>
2020-07-11 20:05:39 +0300

    no message

--- Calis/ViewController.swift
+++ Calis/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lessonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    let lessonsArray = ["Turkish", "Mathematics", "Geometry", "Physics", "Chemistry", "Biology", "History", "Geography", "Philosophy"]
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        lessonLabel.text = lessonsArray.randomElement()
+    }
+    
 }
 
